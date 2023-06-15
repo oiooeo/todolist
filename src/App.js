35bd6cc91ab222d326252aa34331ce2a679778d9
Choi Yoonseo <choiys1103@naver.com>
2023-06-15 12:07:54 +0900

    Feat02: 투두리스트 아이템 삭제 기능 추가
    Buttons 컴포넌트 분리해서 사용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,115 @@
 import "./App.css";
+import { useState } from "react";
+import Input from "./components/Input";
+import DeleteButton from "./components/Buttons";
 
 function App() {
-  return <div></div>;
+  const [todoList, setTodoList] = useState([
+    {
+      id: 0,
+      title: "리액트 공부하기",
+      content: "리액트 입문 강의 듣기",
+      isDone: true,
+    },
+    {
+      id: 1,
+      title: "리액트 공부하기",
+      content: "리액트 숙련 강의 듣기",
+      isDone: false,
+    },
+    {
+      id: 2,
+      title: "리액트 공부하기",
+      content: "리액트 숙련 강의 듣기",
+      isDone: false,
+    },
+  ]);
+
+  const [todo, setTodo] = useState({
+    title: "",
+    content: "",
+  });
+
+  const setValue = (event) => {
+    const { name, value } = event.target;
+
+    setTodo({ ...todo, [name]: value });
+  };
+
+  const clickAddButtonHandler = (event) => {
+    event.preventDefault();
+    const newTodoList = {
+      id: todoList[todoList.length - 1]?.id + 1 || 1,
+      ...todo,
+      isDone: false,
+    };
+
+    setTodoList([...todoList, newTodoList]);
+    setTodo({ title: "", content: "" });
+  };
+
+  const clickDeleteButtonHandler = (id) => {
+    const newTodoList = todoList.filter((item) => item.id !== id);
+    setTodoList(newTodoList);
+  };
+
+  const workingTask = todoList.filter((todoList) => !todoList.isDone);
+  const doneTask = todoList.filter((todoList) => todoList.isDone);
+
+  return (
+    <div className="layout">
+      <header className="header">MY TO-DO LIST</header>
+
+      <Input
+        clickAddButtonHandler={clickAddButtonHandler}
+        todo={todo}
+        setValue={setValue}
+      ></Input>
+
+      <div>
+        <p className="taskState">진행중 🔥</p>
+        <div className="list">
+          {workingTask.map(function (item) {
+            return (
+              <div key={item.id} className="todo">
+                <p className="title">{item.title}</p>
+                <p className="content">{item.content}</p>
+                <br />
+                <DeleteButton
+                  item={item}
+                  clickDeleteButtonHandler={clickDeleteButtonHandler}
+                >
+                  삭제
+                </DeleteButton>
+                <button className="updateBtn">완료</button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+      <div>
+        <p className="taskState">완료 ✨</p>
+        <div className="list">
+          {doneTask.map(function (item) {
+            return (
+              <div key={item.id} className="todo">
+                <p className="title">{item.title}</p>
+                <p className="content">{item.content}</p>
+                <br />
+                <DeleteButton
+                  item={item}
+                  clickDeleteButtonHandler={clickDeleteButtonHandler}
+                >
+                  삭제
+                </DeleteButton>
+                <button className="updateBtn">취소</button>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
